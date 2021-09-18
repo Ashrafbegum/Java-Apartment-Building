0aed98dc50bb953e70423c4c12e889fba29c1ed1
Ashraf <ashrafunnisa931@gmail.com>
2021-09-19 01:03:29 +1000

    Added Apartment.java class and implemented collectData() of Room.java

---
+++ Java-Apartment-Building/src/com/myprojects/Apartment.java
@@ -0,0 +1,13 @@
+package com.myprojects;
+
+public class Apartment extends Room{
+	private String owner;
+	Apartment(int id, String owner) {
+		super(id);
+		this.owner = owner;
+	}
+	
+	public String collectData() {
+			 return "Apartment Details:\n ID: " + getId() + "Owner: " + owner + " Temperature: " + getTemperature() + " Heating Enabled: " + isHeatingEnabled() + " Cooling Enabled: " + isCoolingEnabled();
+	}
+}
