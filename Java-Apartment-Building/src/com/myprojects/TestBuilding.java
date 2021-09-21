33fbcd31ff37654bb28dbc73fcac3c9ad1681a8b
Ashraf <ashrafunnisa931@gmail.com>
2021-09-21 13:46:04 +1000

    Added testCollectData() to test all rooms display collectData() properly.

--- Java-Apartment-Building/src/com/myprojects/TestBuilding.java
+++ Java-Apartment-Building/src/com/myprojects/TestBuilding.java
@@ -1,5 +1,7 @@
 package com.myprojects;
 
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -34,6 +36,39 @@ public class TestBuilding {
 		
 		Assert.assertEquals(true, room4.isHeatingEnabled());
 		Assert.assertEquals(false, room4.isCoolingEnabled());
+		
+	}
+	
+	@Test
+	public void testCollectData() {
+		Building building = new Building(25);
+		
+		Room room1 = new Apartment(101, "Ashraf");
+		Room room2 = new Apartment(102, "Khan");
+		Room room3 = new CommonRoom(201, RoomType.GYM);
+		Room room4 = new CommonRoom(202, RoomType.LIBRARY);
+	
+		room1.setTemperature(27);
+		room2.setTemperature(17);
+		room3.setTemperature(30);
+		room2.setTemperature(20);
+		
+		building.addRoom(room1);
+		building.addRoom(room2);
+		building.addRoom(room3);
+		building.addRoom(room4);
+		
+		String resultString1 = "Apartment - ID: 101, Owner: Ashraf, Temperature: 27, Heating Enabled: false, Cooling Enabled: true";
+		Assert.assertEquals(resultString1, room1.collectData());
+		
+		String resultString2 = "Apartment - ID: 102, Owner: Khan, Temperature:17, Heating Enabled: true, Cooling Enabled: false";
+		Assert.assertEquals(resultString2, room1.collectData());
+		
+		String resultString3 = "Common Room - ID: 201, Room Type: GYM, Temperature: 30, Heating Enabled: false, Cooling Enabled: true";
+		Assert.assertEquals(resultString3, room1.collectData());
+		
+		String resultString4 = "Common Room - ID: 202, Room Type: LIBRARY, Temperature: 20, Heating Enabled: true, Cooling Enabled: false";
+		Assert.assertEquals(resultString4, room1.collectData());
 	}
 	
 	@Test 
