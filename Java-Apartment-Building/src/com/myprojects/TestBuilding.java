1ad64bdbcf678946808e59c3ad2ad81cca09e711
Ashraf <ashrafunnisa931@gmail.com>
2021-09-21 11:48:43 +1000

    Installed TestNG plug in to test and tested sample test to verify test is executed.

---
+++ Java-Apartment-Building/src/com/myprojects/TestBuilding.java
@@ -0,0 +1,22 @@
+package com.myprojects;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestBuilding {
+//	Building building = new Building(25);
+//	
+//	Apartment apt = new Apartment(101, "Ashraf");
+//	Apartment apt2 = new Apartment(102, "Khan");
+//
+//	
+//	@Test
+//	   public void testCollectData() {
+//		String str = apt.collectData();
+//		Assert.assertEquals(101, "Ashraf", )
+//	}
+	@Test 
+	public void testing() {
+		System.out.println("testing executed");
+	}
+}
