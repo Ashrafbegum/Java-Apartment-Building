f6e0809544310769bfb3c72bf5068e19b5b707a3
Ashraf <ashrafunnisa931@gmail.com>
2021-09-21 13:20:32 +1000

    Added a test to check Heating Enabled and Cooling Enabled set properly on given setpoint and temperature for all rooms.

--- Java-Apartment-Building/src/com/myprojects/TestBuilding.java
+++ Java-Apartment-Building/src/com/myprojects/TestBuilding.java
@@ -4,17 +4,38 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestBuilding {
-//	Building building = new Building(25);
-//	
-//	Apartment apt = new Apartment(101, "Ashraf");
-//	Apartment apt2 = new Apartment(102, "Khan");
-//
-//	
-//	@Test
-//	   public void testCollectData() {
-//		String str = apt.collectData();
-//		Assert.assertEquals(101, "Ashraf", )
-//	}
+	@Test
+	public void testHeatCool() {
+		Building building = new Building(25);
+		
+		Room room1 = new Apartment(101, "Ashraf");
+		Room room2 = new Apartment(102, "Khan");
+		Room room3 = new CommonRoom(201, RoomType.GYM);
+		Room room4 = new CommonRoom(202, RoomType.LIBRARY);
+		
+		room1.setTemperature(27);
+		room2.setTemperature(17);
+		room3.setTemperature(30);
+		room2.setTemperature(20);
+
+		building.addRoom(room1);
+		building.addRoom(room2);
+		building.addRoom(room3);
+		building.addRoom(room4);
+		
+		Assert.assertEquals(false, room1.isHeatingEnabled());
+		Assert.assertEquals(true, room1.isCoolingEnabled());
+		
+		Assert.assertEquals(true, room2.isHeatingEnabled());
+		Assert.assertEquals(false, room2.isCoolingEnabled());
+		
+		Assert.assertEquals(false, room3.isHeatingEnabled());
+		Assert.assertEquals(true, room3.isCoolingEnabled());
+		
+		Assert.assertEquals(true, room4.isHeatingEnabled());
+		Assert.assertEquals(false, room4.isCoolingEnabled());
+	}
+	
 	@Test 
 	public void testing() {
 		System.out.println("testing executed");
